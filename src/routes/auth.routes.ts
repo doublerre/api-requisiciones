6626beae2f92f0ab60d4b2dec11e4ff6c8fca816
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@outlook.com>
2023-01-16 20:11:08 -0600

    Solo se usara para crear nuevos usuarios

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,7 +4,7 @@ const router = Router();
 import {save, login} from '../controllers/user.controller';
 import { loginUser } from '../requests/userRequest';
 
-router.post('/register', save);
+//router.post('/register', save);
 router.post('/login', loginUser, login);
 
 export default router;
