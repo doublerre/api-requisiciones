5a43ef324393b908b1cddd14ed71c3867aaf5340
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@outlook.com>
2023-01-16 20:15:58 -0600

    Eliminacion de lineas innecesarias

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,7 +3,6 @@ import { Router } from "express";
 const router = Router();
 
 import {profile, updateProfile, changePassword} from '../controllers/user.controller';
-import { checkValidationUser } from "../middlewares/Roles";
 import {TokenValidation} from '../middlewares/verifyToken';
 import { updateProfileRequest, changePasswordRequest } from "../request/userRequest";
 
