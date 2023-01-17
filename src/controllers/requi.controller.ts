f3de570e2950fff2ef1fb60ac628164a8494772a
Alfonso Rafael Solis Rangel <asolis@cozcyt.gob.mx>
2023-01-17 12:36:44 -0600

    Rutas para requisiciones

---
+++ src/controllers/requi.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+
+import Requi from "../models/Requi";
+
+export const save = async(req: Request, res: Response): Promise<Response> => {
+    const newRequi = new Requi(req.body);
+
+    await newRequi.save();
+
+    return res.status(201).json({message: '', data: newRequi})
+}
+
+export const requis  = async(req: Request, res: Response): Promise<Response> => {
+    const requis = await Requi.find({estatus: req.body.estatus});
+    if(!requis) return res.status(404).json({message: 'No se han encontrado requisiciones.'});
+
+    return res.status(200).json({message: 'Consulta exitosa.', data: requis});
+}
--- src/routes/requi.routes.ts
+++ src/routes/requi.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import {save} from '../controllers/requi.controller';
+
 const router = Router();
+
+router.post('/requi', save);
 
 export default router;
