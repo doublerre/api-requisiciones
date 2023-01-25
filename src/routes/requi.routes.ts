e502e55d3a14d299807ab95900d6914f62ba0086
Alfonso Rafael Solis Rangel <asolis@cozcyt.gob.mx>
2023-01-25 08:38:19 -0600

    Token requerido

--- src/routes/requi.routes.ts
+++ src/routes/requi.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 
 import {deleteRequi, save, updateRequi, uploadFile} from '../controllers/requi.controller';
+import { TokenValidation } from "../middlewares/verifyToken";
 
 import multer from '../libs/multer';
 
 const router = Router();
 
-router.post('/requi', save);
-router.put('/requi/file/:id', multer.single('pdfFile'), uploadFile);
-router.delete('/requi/:id', deleteRequi);
-router.put('requi/:id', updateRequi);
+router.post('/requi', [TokenValidation], save);
+router.put('/requi/file/:id', [TokenValidation], multer.single('pdfFile'), uploadFile);
+router.delete('/requi/:id', [TokenValidation], deleteRequi);
+router.put('requi/:id', [TokenValidation], updateRequi);
 
 export default router;
