696c66a14a44f2e84b01ce2bc8e330798c9333b0
Doublerre <alfonsorafaelsolisrangel@gmail.com>
2023-02-08 10:56:36 -0600

    Implementando validacion de request

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -4,7 +4,8 @@ const router = Router();
 import { save } from "../controllers/user.controller";
 import { checkDuplicateUsername, verifyRoles } from "../middlewares/Role";
 import { TokenValidation } from "../middlewares/verifyToken";
+import { createUser } from "../requests/userRequest";
 
-router.post('/admin/create-user', [TokenValidation, checkDuplicateUsername, verifyRoles("admin")], save);
+router.post('/admin/create-user', [TokenValidation, checkDuplicateUsername, verifyRoles("admin"), createUser], save);
 
 export default router;
